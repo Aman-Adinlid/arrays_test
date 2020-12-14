76f8a7f69d335534cadf37ce15062f78e2f331b0
aman adinlid <95pinkpanda@gmail.com>
2020-12-15 00:09:21 +0100

    Initial commit

---
+++ src/main/java/org/example/array.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class array {
+
+    public static void main(String[] args) {
+        String[] stringArray = new String[3];
+        int intArray[] = new int[3];
+        double[] doubleArray = new double[3];
+        
+        //int[] numbers= new int[]{2,5,6,8,9,11,100};
+        String[] names = new String[]{"Erik", "Ulf", "Simon"};
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+    }
+}
