8253aef707bd463aafbc821283e95f2ebc234ad2
aman adinlid <95pinkpanda@gmail.com>
2020-12-16 18:40:55 +0100

    array practice

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,32 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+
+public class App {
+
+    private String[] names = {"Aman","Adam","Jacob","Sam","smith"};
+
+    public String findName(String NameToFind) {
+        for (String name : names) {
+            if (name.equalsIgnoreCase(NameToFind)) {
+                return name;
+            }
+        }
+       return  "Error: No match";
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Search by name: ");
+        String name = scanner.nextLine();
+
+        App app = new App();
+        String findName = app.findName(name);
+        System.out.println(findName);
+        System.out.println(app.findName("aman"));
+        System.out.println(app.findName("adam"));
+        System.out.println(app.findName("smith"));
+
     }
 }
--- src/main/java/org/example/array.java
+++ src/main/java/org/example/array.java
@@ -3,45 +3,49 @@ package org.example;
 public class array {
 
     public static void main(String[] args) {
-        int [] array ={1,2,3,4,5,7,8,0};
+        int[] array = {1, 2, 3, 4, 5, 7, 8, 0};
         int value = array[0];
-        int array2 = array [1];
-        int array3 = array [2];
-        int array4 = array [3];
+        int array2 = array[1];
+        int array3 = array[2];
+        int array4 = array[3];
         System.out.println(value);
         System.out.println(array2);
         System.out.println(array3);
         System.out.println(array4);
-       // System.out.println(array.length);
+        // System.out.println(array.length);
 
 
-        char [] word = {'a','m','a','n'};
-       // System.out.println(word);
+        char[] word = {'a', 'm', 'a', 'n'};
+        // System.out.println(word);
 
-       // int numbers [] = {1,2,4,6,7};
-        String[] names = new String[] {"erik","adam","yousif"};
+        // int numbers [] = {1,2,4,6,7};
+        String[] names = new String[]{"erik", "adam", "yousif"};
         System.out.println(names[0]);
         System.out.println(names[1]);
         System.out.println(names[2]);
         System.out.println(names.length);
 
-int numbers [] = {0,1,2,3};
+        int numbers[] = {0, 1, 2, 3};
         String carName[] = new String[3];
         carName[0] = "VOLVO";
         carName[1] = "bosh";
         // System.out.println(carName[3]);
 
-         int [] numbers1 = {5,2,3,4,7};
-         System.out.println(numbers1[4]);
-         System.out.println(numbers1[0]);
-         System.out.println(numbers1[2]);
+        int[] numbers1 = {5, 2, 3, 4, 7};
+        System.out.println(numbers1[4]);
+        System.out.println(numbers1[0]);
+        System.out.println(numbers1[2]);
 
-         double[] numbers2 = {1.1,1.2,2.4,3.4};
-         System.out.println(numbers2[0]);
-         System.out.println(numbers2[2]);
-         System.out.println(numbers2[4]);
 
-         String [][] board =new String[4][4];
+        double[] numbers2 = {1.1, 1.2, 2.4, 3.4};
+        System.out.println(numbers2[0]);
+        System.out.println(numbers2[2]);
+        System.out.println(numbers2[4]);
+
+    }
+}
+
+
          
 
 
@@ -53,5 +57,5 @@ int numbers [] = {0,1,2,3};
 
 
 
-    }
-}
+
+
