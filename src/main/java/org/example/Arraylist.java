a981309414a272d51e009889529bd5108bcfbf83
aman adinlid <95pinkpanda@gmail.com>
2020-12-16 09:57:13 +0100

    array practice

---
+++ src/main/java/org/example/Arraylist.java
@@ -0,0 +1,8 @@
+package org.example;
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static void main (String [] args) {
+
+    }
+}
--- src/main/java/org/example/array.java
+++ src/main/java/org/example/array.java
@@ -3,13 +3,55 @@ package org.example;
 public class array {
 
     public static void main(String[] args) {
-        String[] stringArray = new String[3];
-        int intArray[] = new int[3];
-        double[] doubleArray = new double[3];
-        
-        //int[] numbers= new int[]{2,5,6,8,9,11,100};
-        String[] names = new String[]{"Erik", "Ulf", "Simon"};
+        int [] array ={1,2,3,4,5,7,8,0};
+        int value = array[0];
+        int array2 = array [1];
+        int array3 = array [2];
+        int array4 = array [3];
+        System.out.println(value);
+        System.out.println(array2);
+        System.out.println(array3);
+        System.out.println(array4);
+       // System.out.println(array.length);
+
+
+        char [] word = {'a','m','a','n'};
+       // System.out.println(word);
+
+       // int numbers [] = {1,2,4,6,7};
+        String[] names = new String[] {"erik","adam","yousif"};
         System.out.println(names[0]);
         System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names.length);
+
+int numbers [] = {0,1,2,3};
+        String carName[] = new String[3];
+        carName[0] = "VOLVO";
+        carName[1] = "bosh";
+        // System.out.println(carName[3]);
+
+         int [] numbers1 = {5,2,3,4,7};
+         System.out.println(numbers1[4]);
+         System.out.println(numbers1[0]);
+         System.out.println(numbers1[2]);
+
+         double[] numbers2 = {1.1,1.2,2.4,3.4};
+         System.out.println(numbers2[0]);
+         System.out.println(numbers2[2]);
+         System.out.println(numbers2[4]);
+
+         String [][] board =new String[4][4];
+         
+
+
+
+
+
+
+
+
+
+
     }
 }
